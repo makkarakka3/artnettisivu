3e575614f209f14c84f073820ff0c4f27c1d29e3
makkarakka3 <49957998+makkarakka3@users.noreply.github.com>
2022-10-12 21:25:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+    function toggleCloudRedirectAlert() {
+        if (confirm("Tämä uudelleenohjaa sinut cloud.artelokuvaoy.com:iin!\nJos et ole töissä Art Elokuva OY:llä sinulla ei ole käyttäjää tälle sivulle.\nJos olet töissä Art Elokuva OY:llä ja halua käyttäjän, ota yhteys tatuun.\nHaluatko jatkaa?")) {
+            location.replace("https://cloud.artelokuvaoy.com")
+    } else {
+        void(0)
+    }
+}
